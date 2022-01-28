9debde6771fec04ffd88a330ea0c937feb64f8a9
George Wizeman <moxorama@yandex.ru>
2022-01-28 15:06:43 +0300

    Testcase example

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppState, AppStateStatus } from 'react-native';
 
 export default function App() {
+  const handleBlur = (state: AppStateStatus) => {
+    console.log(state);
+  }
+
+  const handleFocus = (state: AppStateStatus) => {
+    console.log(state);
+  }
+
+  useEffect(() => {
+    AppState.addEventListener('blur', handleBlur);
+    AppState.addEventListener('focus', handleFocus);
+  },[]);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
